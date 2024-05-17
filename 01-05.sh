e2834e8a3fee68df343f3c0b57d7b53499a5ba25
Guille <51679006+GuiSchet@users.noreply.github.com>
2024-05-17 17:10:31 +0000

    123

---
+++ 01-05.sh
@@ -0,0 +1,15 @@
+#005.sh In this exercise, we aim to show how absolute timelock works.
+#    Create a Transaction with an absolute timelock of 150, this transaction will fund the multisig address with 100 BTC from Trader and the change-back to Trader accounting for fees
+#    Sign and broadcast the transaction.
+#    Your final command should answer the question: What is the final line of command ´bitcoin-cli sendrawtransaction txhex´?
+
+bitcoin-cli createwallet "Test"
+Alice=$(bitcoin-cli getnewaddress)
+Bob=$(bitcoin-cli getnewaddress)
+Alice_pubkey=$(bitcoin-cli -named getaddressinfo address=$Alice | jq -r '.pubkey')
+echo $Alice_pubkey
+Bob_pubkey=$(bitcoin-cli -named getaddressinfo address=$Bob | jq -r '.pubkey')
+echo $Bob_pubkey
+multisig=$(bitcoin-cli createmultisig 2 "[\"$Alice_pubkey\",\"$Bob_pubkey\"]")
+multisig_address=$(multisig | jq -r '.address')
+echo $multisig_address
